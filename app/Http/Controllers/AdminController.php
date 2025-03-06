b9292dbe32004a4a5e9343ba7b56c74b6599e6d6
sy <sypham040503@gmail.com>
2025-03-07 00:43:23 +0700

    category

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+use DB;
+use Session;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    public function index()
+    {
+        return view('admin_login');
+    }
+    public function show_dashboard()
+    {
+        return view('admin.dashboard');
+    }
+    public function dashboard(Request $request)
+    {
+        $admin_email = $request->admin_email;
+        $admin_password = md5($request->admin_password);
+        $result = DB::table('tbl_admin')->where('admin_email', $admin_email)->where('admin_password', $admin_password)->first();
+        if($result){
+            Session::put('admin_name', $result->admin_name);
+            Session::put('admin_id', $result->admin_id);
+            return Redirect::to('/dashboard');
+        }else{
+            Session::put('message', 'Mật khẩu hoặc tài khoản không đúng');
+            return Redirect::to('/admin');
+        }
+    }
+    public function logout()
+    {
+        Session::put('admin_name', null);
+        Session::put('admin_id', null);
+        return Redirect::to('/admin');
+    }
+}
